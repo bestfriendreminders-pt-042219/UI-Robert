3337271575fe31b9cb5009cf211b56fa90f58e9a
Robert Kulp <pedalinon@gmail.com>
2019-04-26 13:07:38 -0700

    Classes corresponding less files created.

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+// Footer class
+class Footer {
+	constructor(footer){
+		this.footer = footer;
+
+		footer = document.querySelector('footer')
+		footer.style.height = '200px';
+		footer.style.backgroundColor = '#090707';
+		console.log(footer)
+	}
+}
+
+footer = new Footer();
