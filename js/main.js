bdf9628d02c73fb7214ff6ff11dcf3e7cc1800ed
Robert Kulp <pedalinon@gmail.com>
2019-04-26 18:54:44 -0700

    Now to duplicate the token class object as needed. Should be interesting

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ class Main {
 		main.className = 'main';
 		console.log(main)
 
+// FIgure out how to insert before. Will come back to. 
 		const titleSection = document.querySelector('#mainTitleSection');
 		const mainTitle = document.createElement('h1');
 // console.log(titleSection)
@@ -15,7 +16,7 @@ class Main {
 		// titleSection.className = 'mainTitleSection';
 		mainTitle.textContent = 'Your calendar at your fingertips!';
 
-		// section.appendChild(mainTitle)
+
 	}
 }
 
